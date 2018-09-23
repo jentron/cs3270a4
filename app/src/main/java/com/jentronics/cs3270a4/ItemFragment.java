2f705c4c0efd27ea6a1282b42fadf33de20bee2a
Ron Jensen <ron@jentronics.com>
2018-09-23 13:24:55 -0600

    Mostly change from EditText to TextInput

--- app/src/main/java/com/jentronics/cs3270a4/ItemFragment.java
+++ app/src/main/java/com/jentronics/cs3270a4/ItemFragment.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.math.BigDecimal;
 
@@ -16,6 +20,8 @@ import java.math.BigDecimal;
  */
 public class ItemFragment extends Fragment {
     private BigDecimal itemAmount = new BigDecimal(100);
+    private View root;
+    private EditText item1;
 
     public ItemFragment() {
         // Required empty public constructor
@@ -25,7 +31,32 @@ public class ItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item, container, false);
+        return root = inflater.inflate(R.layout.fragment_item, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        item1 = (EditText) root.findViewById(R.id.item1);
+        TextWatcher amountWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                Log.d("test", "Text change to: " + s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.d("test", "afterTextChanged: " + editable.toString());
+            }
+        };
+
+        item1.addTextChangedListener(amountWatcher);
     }
 
     public BigDecimal getItemAmount() {
